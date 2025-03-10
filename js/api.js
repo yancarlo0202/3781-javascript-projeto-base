5df655fcfde818a05a092c7553ab4fe2d9b280e9
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-03-10 16:31:29 -0400

    requisições GET e POST

--- js/api.js
+++ js/api.js
@@ -7,6 +7,21 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+        } catch {
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
     }
 }
 
--- js/ui.js
+++ js/ui.js
@@ -6,18 +6,35 @@ const ui = {
 
         try {
             const pensamentos = await api.buscarPensamentos()
-            pensamentos.forEach(pensamento => {
-                listaPensamentos.innerHTML += `
-                    <li class="li-pensamento" data-id="${pensamento.id}">
-                    <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-                    <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-                    <div class="pensamento-autoria">${pensamento.autoria}</div>
-                    </li>
-                `
-            })
+            pensamentos.forEach(ui.adicionarPensamentoNaLista)
         } catch {
             alert('Erro ao renderizar pensamentos')
         }
+    },
+
+    adicionarPensamentoNaLista(pensamento){
+        const listaPensamentos = document.getElementById("lista-pensamentos")
+        const li = document.createElement("li")
+        li.setAttribute("data-id", pensamento.id)
+        li.classList.add("li-pensamento")
+
+        const iconeAspas = document.createElement("img")
+        iconeAspas.src = "assets/imagens/aspas-azuis.png"
+        iconeAspas.alt = "Aspas azuis"
+        iconeAspas.classList.add("icone-aspas")
+
+        const pensamentoConteudo = document.createElement("div")
+        pensamentoConteudo.textContent = pensamento.conteudo
+        pensamentoConteudo.classList.add("pensamento-conteudo")
+        
+        const pensamentoAutoria = document.createElement("div")
+        pensamentoAutoria.textContent = pensamento.autoria
+        pensamentoAutoria.classList.add("pensamento-autoria")
+
+        li.appendChild(iconeAspas)
+        li.appendChild(pensamentoConteudo)
+        li.appendChild(pensamentoAutoria)
+        listaPensamentos.appendChild(li)
     }
 }
 
