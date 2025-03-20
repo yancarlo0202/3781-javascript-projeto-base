a9541aa5363d7c094eff28b94ca8cd274eda8ca2
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-03-20 16:36:03 -0400

    manipulando e exibindo datas

--- js/api.js
+++ js/api.js
@@ -1,10 +1,21 @@
 const URL_BASE = "http://localhost:3000"
 
+const converterStringParaData = (dataString) => {
+  const[anos, mes, dia] = dataString.split("-")
+  return new Date(Date.UTC(anos, mes - 1, dia))
+}
+
 const api = {
   async buscarPensamentos() {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos`)
-      return await response.data
+      const pensamentos =  await response.data
+      return pensamentos.map(pensamento => {
+        return {
+          ...pensamento,
+          data: new Date(pensamento.data)
+        }
+      })
     }
     catch {
       alert('Erro ao buscar pensamentos')
@@ -14,7 +25,11 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+      const data = converterStringParaData(pensamento.data)
+      const response = await axios.post(`${URL_BASE}/pensamentos`, {
+        ...pensamento,
+        data: data.toISOString()
+      })
       return await response.data
     }
     catch {
@@ -26,7 +41,11 @@ const api = {
   async buscarPensamentoPorId(id) {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-      return await response.data
+      const pensamento = await response.data
+      return {
+        ...pensamento,
+        data: new Date(pensamento.data)
+      }
     }
     catch {
       alert('Erro ao buscar pensamento')
--- js/ui.js
+++ js/ui.js
@@ -7,6 +7,8 @@ const ui = {
     document.getElementById("pensamento-id").value = pensamento.id
     document.getElementById("pensamento-conteudo").value = pensamento.conteudo
     document.getElementById("pensamento-autoria").value = pensamento.autoria
+    document.getElementById("pensamento-data").value = pensamento.data.toISOString().split("T")[0]
+    document.getElementById("form-container").scrollIntoView()
   },
 
   limparFormulario() {
@@ -58,6 +60,18 @@ const ui = {
     pensamentoAutoria.textContent = pensamento.autoria
     pensamentoAutoria.classList.add("pensamento-autoria")
 
+    const pensamentoData = document.createElement("div")
+    var options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+        timeZone: 'UTC'
+    }
+    const dataFormatada = pensamento.data.toLocaleDateString('pt-BR', options)
+    pensamentoData.textContent = dataFormatada
+    pensamentoData.classList.add("pensamento-data")
+
     const botaoEditar = document.createElement("button")
     botaoEditar.classList.add("botao-editar")
     botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
@@ -108,6 +122,7 @@ const ui = {
     li.appendChild(iconeAspas)
     li.appendChild(pensamentoConteudo)
     li.appendChild(pensamentoAutoria)
+    li.appendChild(pensamentoData)
     li.appendChild(icones)
     listaPensamentos.appendChild(li)
   }
